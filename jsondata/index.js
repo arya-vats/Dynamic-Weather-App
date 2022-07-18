03489dc508a32e2e94873e23c16753d6364b8d80
Arya-Vats <89343269+arya-vats@users.noreply.github.com>
2022-07-18 16:52:55 +0530

    Delete jsondata directory

--- jsondata/index.js
+++
@@ -1,7 +0,0 @@
-const data={
-    name: "Arya",
-    age: 21
-}
-
-const jsondata = JSON.stringify(data); //this is used to convert object into json data.
-const revdata = JSON.parse(jsondata);// This is used to convert json data into original data.
